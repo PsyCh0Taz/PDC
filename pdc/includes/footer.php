0aff9f2a254fa3d62f0415cd3d058d2b2f1ae437
PsyCh0Taz <45539034+PsyCh0Taz@users.noreply.github.com>
2026-07-22 19:38:35 +0200

    .

--- pdc/includes/footer.php
+++ pdc/includes/footer.php
@@ -1,6 +1,8 @@
 <?php // includes/footer.php ?>
 </div><!-- /.pdc-main-wrapper -->
 
+<div class="pdc-signature-fixed">VibeCoding by C²Factory</div>
+
 <script src="<?php echo APP_URL; ?>/assets/js/bootstrap.bundle.min.js"></script>
 <script src="<?php echo APP_URL; ?>/assets/js/fontawesome.min.js"></script>
 <script src="<?php echo APP_URL; ?>/assets/js/jquery.min.js"></script>
